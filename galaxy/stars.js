025d8cf26abd26cbc20d294ae43c4db874cbb255
Pradipta Dey <pradiptadey@Pradiptas-MacBook-Pro.local>
2026-02-16 23:22:14 +0530

    Adds galaxy project using html-css

---
+++ galaxy/stars.js
@@ -0,0 +1,28 @@
+const stars = (length) => {
+  const pOfStars = [];
+  for (let i = 100; i < length; i++) {
+    const star = `#star-${i} {
+  position: fixed;
+  top: ${random(0, 100)}%;
+  left: ${random(0, 100)}%;
+}`;
+    pOfStars.push(star);
+  }
+
+  return pOfStars.join("\n\n");
+};
+
+const random = (low, high) => Math.floor(low + (Math.random() * (high - low)));
+
+// console.log(stars(400));
+
+const starsHtml = (length) => {
+  const stars = ["⋆", "✦", "✧", "⭑", "★", "☆", "✩", "✬"];
+  const paras = [];
+  for (let i = 400; i < length; i++) {
+    paras.push(`<p id="star-${i}">${stars[random(0, stars.length)]}</p>`);
+  }
+  return paras.join("\n");
+};
+
+console.log(starsHtml(500));
